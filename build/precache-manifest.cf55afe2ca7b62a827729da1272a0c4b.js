d44fb28400d61351da4ac576873c8efcdcaea826
Chris Del <drumgod101@gmail.com>
2019-03-10 19:15:08 -0400

    add build

---
+++ build/precache-manifest.cf55afe2ca7b62a827729da1272a0c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "926fa95f4fa65fbf9cc5",
+    "url": "/static/js/main.72220108.chunk.js"
+  },
+  {
+    "revision": "d4ddf75d12f491179466",
+    "url": "/static/js/2.7158b270.chunk.js"
+  },
+  {
+    "revision": "926fa95f4fa65fbf9cc5",
+    "url": "/static/css/main.7732a07e.chunk.css"
+  },
+  {
+    "revision": "ced967c476d4998d9f8d58f019cd957d",
+    "url": "/index.html"
+  }
+];
